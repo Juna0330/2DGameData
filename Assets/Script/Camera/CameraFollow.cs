dd7224fa8d3611c3af57d001c7f6a354ff099cab
Masaki Onara <ottoaizu330@gmail.com>
2021-04-18 22:37:06 +0900

    初期設定

---
+++ Assets/Script/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   public Transform target;
+   public Vector3 offsets;
+   [Range(1, 10)] public float smoothFactor;
+
+   private void FixedUpdate()
+   {
+      Follow();
+   }
+
+   void Follow()
+   {
+      Vector3 targetPosition = target.position + offsets;
+      Vector3 smoothPosition=Vector3.Lerp(transform.position,targetPosition,smoothFactor*Time.fixedDeltaTime);
+   }
+}
---
+++ Assets/Script/Enemies/EnemySpecific/Enemy1/State Machine/Entity.cs
@@ -0,0 +1,171 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+
+    public FiniteStateMachine stateMachine;
+
+    public D_Entity entityData;
+
+    public int facingDirection { get; private set; }
+    public Rigidbody2D rb { get; private set; }
+    public Animator anim { get; private set; }
+
+    public GameObject aliveGo { get; private set; }
+    public AnimationToStatemachine atsm { get; private set; }
+
+    public int lastDamageDirection { get; private set; }
+
+    [SerializeField]
+    private Transform wallCheck;
+    [SerializeField]
+    private Transform ledgeCheck;
+    [SerializeField]
+    private Transform playerCheck;
+    [SerializeField]
+    private Transform groundCheck;
+
+    private float currentHealth;
+    private float currentStunResistance;
+    private float lastDamageTime;
+
+   
+
+    private Vector2 velocityWorkspace;
+
+    protected bool isStunned;
+    protected bool isDead;
+
+    public virtual void Start()
+    {
+        facingDirection = 1;
+        currentHealth = entityData.maxHealth;
+        currentStunResistance = entityData.stunResistance;
+        aliveGo = transform.Find("Alive").gameObject;
+        rb = aliveGo.GetComponent<Rigidbody2D>();
+        anim = aliveGo.GetComponent<Animator>();
+        atsm = aliveGo.GetComponent<AnimationToStatemachine>();
+
+        stateMachine = new FiniteStateMachine();
+    }
+
+    public virtual void Update()
+    {
+        stateMachine.currentState.LogicUpdate();
+
+        anim.SetFloat("yVelocity", rb.velocity.y);
+
+        if (Time.time >= lastDamageTime + entityData.stunRecoveryTime)
+        {
+            ResetStunResistance();
+        }
+    }
+
+    public virtual void FixedUpdate()
+    {
+        stateMachine.currentState.PhyisicsUpdate();
+    }
+
+    public virtual void SetVelocity(float velocity)
+    {
+        velocityWorkspace.Set(facingDirection * velocity, rb.velocity.y);
+        rb.velocity = velocityWorkspace;
+    }
+    public virtual void SetVelocity(float velocity,Vector2 angle,int direciton)
+    {
+        angle.Normalize();
+        velocityWorkspace.Set(angle.x * velocity * direciton, angle.y * velocity);
+        rb.velocity = velocityWorkspace;
+    }
+
+    public virtual bool CheckWall ()
+    {
+        return Physics2D.Raycast(wallCheck.position, aliveGo.transform.right, 
+            entityData.wallCheckDistance, entityData.whatIsGround);
+    }
+
+    public virtual bool CheckLedge()
+    {
+        return Physics2D.Raycast(ledgeCheck.position, Vector2.down, entityData.ledgeCheckDistance,
+            entityData.whatIsGround);
+    }
+    public virtual bool CheckGround()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, entityData.groundCheckRadius, entityData.whatIsGround);
+    }
+
+    public virtual bool CheckPlayerInMinAgroRange()
+    {
+        return Physics2D.Raycast(playerCheck.position, aliveGo.transform.right, entityData.minAgroDistance, entityData.whatIsPlayer);
+    }
+
+    public virtual bool CheckPlayerInMaxAgroRange()
+    {
+        return Physics2D.Raycast(playerCheck.position, aliveGo.transform.right, entityData.maxAgroDistance, entityData.whatIsPlayer);
+    }
+
+    public virtual bool CheckPlayerInCloseRangeAction()
+    {
+        return Physics2D.Raycast(playerCheck.position, aliveGo.transform.right, entityData.closeRangeActionDistance, entityData.whatIsPlayer);
+    }
+
+    public virtual void DamageHop(float velocity)
+    {
+        velocityWorkspace.Set(rb.velocity.x, velocity);
+        rb.velocity = velocityWorkspace;
+    }
+
+    public virtual void ResetStunResistance()
+    {
+        isStunned = false;
+        currentStunResistance = entityData.stunResistance;
+    }
+
+
+    public virtual void Damage(AttackDetails attackDetails)
+    {
+        lastDamageTime = Time.time;
+
+        currentHealth -= attackDetails.damageAmount;
+        currentStunResistance -= attackDetails.stunDamageAmount;
+
+        DamageHop(entityData.damageHopSpeed);
+        Instantiate(entityData.hitParticle, aliveGo.transform.position, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
+
+        if (attackDetails.position.x > aliveGo.transform.position.x)
+        {
+            lastDamageDirection = -1;
+        }
+        else
+        {
+            lastDamageDirection = 1;
+        }
+        if (currentStunResistance <= 0)
+        {
+            isStunned = true;
+        }
+        if (currentHealth <= 0)
+        {
+            isDead = true;
+        }
+    }
+    public virtual void Flip()
+    {
+        facingDirection *= -1;
+        aliveGo.transform.Rotate(0f, 180f, 0f);
+    }
+
+    public virtual void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(wallCheck.position, wallCheck.position + (Vector3)(Vector2.right * facingDirection * entityData.wallCheckDistance));
+        Gizmos.DrawLine(ledgeCheck.position, ledgeCheck.position + (Vector3)(Vector2.down * entityData.ledgeCheckDistance));
+
+        Gizmos.DrawWireSphere(playerCheck.position + (Vector3)(Vector2.right * entityData.closeRangeActionDistance), 0.2f);
+        Gizmos.DrawWireSphere(playerCheck.position + (Vector3)(Vector2.right * entityData.maxAgroDistance), 0.2f);
+        Gizmos.DrawWireSphere(playerCheck.position + (Vector3)(Vector2.right * entityData.minAgroDistance), 0.2f);
+    }
+
+}
